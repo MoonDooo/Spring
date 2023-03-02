7fffe9276d04bb9950afa4e73d48b6cdfd429479
MoonDooo <ruddhks9937@gmail.com>
2023-03-02 19:37:05 +0900

    .

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.basic.response;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,5 +11,17 @@ public class ResponseViewController {
                 .addObject("data", "hello!");
 
         return modelAndView;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!");
+
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data", "hello!");
     }
 }
